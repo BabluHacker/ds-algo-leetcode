e7edb59f337b732906c7f9a9226e77dd7ef8e041
Mehedi Hasan <mehedi1055@gmail.com>
2022-05-29 20:29:37 +0600

    Time: 80 ms (33.21%), Space: 14.4 MB (67.07%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp = {}
+        # calculating the char counts
+        for ch in s:
+            if ch in mp:
+                mp[ch] += 1
+            else:
+                mp[ch] = 1
+                
+        for ch in t:
+            if ch not in mp:
+                return False # if not in mp then not a anagram
+            else:
+                mp[ch] -= 1
+                if mp[ch] < 0: return False # if any char is extra here then False
+        
+        # check the mp if any char count is != 0
+        for i in mp.keys():
+            if mp[i] != 0:
+                return False
+        return True
+                
+        
