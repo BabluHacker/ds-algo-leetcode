909e7c04e8a76f73fc9cd1734632dd8be1e1141f
Mehedi Hasan <mehedi1055@gmail.com>
2022-05-29 21:07:31 +0600

    Time: 142 ms (49.17%), Space: 17.2 MB (88.73%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) <= 1:
+            return [strs]
+        result = []
+        sorted_strs = []
+        for s in strs:
+            sorted_strs.append(''.join(sorted(s)))
+        
+        # print(sorted_strs)
+        
+        mp = {} # store the original values of anagram
+        
+        for i in range(len(sorted_strs)):
+            if sorted_strs[i] in mp:
+                mp[sorted_strs[i]].append(strs[i])
+            else:
+                mp[sorted_strs[i]] = [strs[i]]
+        # print(mp)
+        
+        for i in mp.keys():
+            result.append(mp[i])
+        return result
+            
+            
+        
