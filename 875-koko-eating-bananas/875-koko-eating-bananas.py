cba7098d30e2971c95181eb15e7dd59e829f409f
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-04 00:56:00 +0600

    Time: 596 ms (53.70%), Space: 15.5 MB (73.33%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+        
+        while low <= high:
+            m = low + (high-low)//2
+            if self.findHourToEat(piles, m) > h:
+                low = m+1
+            else:
+                high = m-1
+        return low
+            
+    
+    def findHourToEat(self, piles, k):
+        hour = 0
+        for c in piles:
+            hour += math.ceil(c/k)
+        return hour
