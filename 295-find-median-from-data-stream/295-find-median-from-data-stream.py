34d1752b27f6002ebdece3ecc6928e1ec6080bda
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-06 17:55:06 +0600

    Time: 838 ms (38.68%), Space: 35.7 MB (89.47%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,34 +1,37 @@
 class MedianFinder:
 
     def __init__(self):
-        self.small = []
-        self.large = []
+        self.small = [] # max heap
+        self.large = [] # min heap
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -1 * num) # maxheap
-        # making sure every num in small <= num in large
-        if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
+        heapq.heappush(self.small, -1*num)
+        # making sure every elm in small is less than large
+        if self.small and self.large and -1*self.small[0] > self.large[0]:
             tmp = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, tmp)
-        
-        # balance the size of both heap if any one of this is 2 size big
-        if len(self.small) > len(self.large) + 1:
+        # balance 2 heaps
+        if len(self.small) - len(self.large) >= 2 :
             tmp = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, tmp)
-        if len(self.large) > len(self.small) + 1:
+        elif len(self.large) - len(self.small) >= 2:
             tmp = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1 * tmp)
+            heapq.heappush(self.small, -1*tmp)
+    
+        
+        
         
 
     def findMedian(self) -> float:
-        sl = len(self.small)
-        ll = len(self.large)
-        if sl == ll:
-            return (-1*self.small[0] + self.large[0]) / 2
-        elif sl > ll:
+        l1 = len(self.small)
+        l2 = len(self.large)
+        if l1 == l2:
+            return (-1* self.small[0] + self.large[0])/2
+        elif l1 > l2:
             return -1 * self.small[0]
         else:
             return self.large[0]
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
