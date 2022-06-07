405d75b3acd4faa48c01612664d09a3234ea4794
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-08 00:09:40 +0600

    Time: 52 ms (23.07%), Space: 14 MB (11.30%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,8 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         
         dp = {}
-        dp[-1] = 0
-        dp[-2] = 0
+     
         def countWays(n):
             if n == 0:
                 return 1
