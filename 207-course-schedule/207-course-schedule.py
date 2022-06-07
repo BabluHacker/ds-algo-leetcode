1a649cf011b578c342a983c8d582bcc560011728
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-07 20:34:39 +0600

    Time: 199 ms (15.92%), Space: 17.7 MB (7.59%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -6,24 +6,25 @@ class Solution:
         for pre in prerequisites:
             self.g[pre[0]].append(pre[1])
         
-        
-        def has_cycle(crs, visit, recstack): # if cycle found then return True
+        visit, recstack = set(), set()
+        def has_cycle(crs): # if cycle found then return True
             visit.add(crs)
             recstack.add(crs)
             
             for nei in self.g[crs]:
                 if nei not in visit:
-                    if has_cycle(nei, visit, recstack) == True:
+                    if has_cycle(nei) == True:
                         return True
                 elif nei in recstack:
                     return True
             recstack.remove(crs)
             return False
         
-        visit, recstack = set(), set()
+        
         for i in range(numCourses):
-            if has_cycle(i, visit, recstack) == True:
-                return False # can't complete the course as there is a cycle 
+            if i not in visit:
+                if has_cycle(i) == True:
+                    return False # can't complete the course as there is a cycle 
         return True 
             
             
