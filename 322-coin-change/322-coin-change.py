ed6bf2e3956966786c7776dc66ffa4b75e0f7b36
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-08 14:15:52 +0600

    Time: 2477 ms (26.13%), Space: 52.7 MB (5.14%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = {}
+        dp[0] = 0
+        def dfs(n):
+            
+            if n not in dp:
+                min_coin = sys.maxsize 
+                for i in range(len(coins)):
+                    if n-coins[i] >= 0:
+                        min_coin = min(min_coin, 1+dfs(n-coins[i]))
+                dp[n] = min_coin
+            return dp[n]
+        
+        res = dfs(amount)
+        return res if res < sys.maxsize else -1
+            
