309b6881d3742ca355fa7baa3dda7b6bcee69abe
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-11 11:49:58 +0600

    Time: 2814 ms (28.40%), Space: 29.9 MB (62.40%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # monotonically decreasing queue
+        q = collections.deque()
+        
+        l = r = 0
+        res = []
+        while r<len(nums):
+            
+            # from the right pop which values are less than nums[r] bcz it has to be decreasing
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            
+            if l > q[0]: # as q[0] is max and l is moving then just pop left max value
+                q.popleft()
+                
+            if (r+1) >= k:
+                res.append(nums[q[0]])
+                l += 1 # moving window
+            r += 1
+            # print(q, res)
+        return res
+            
+            
+        
