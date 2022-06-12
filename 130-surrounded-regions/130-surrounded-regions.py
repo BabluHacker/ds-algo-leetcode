d2817ce365c8d9aec575b165f59f51433ab81e28
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-12 17:38:33 +0600

    Time: 396 ms (7.79%), Space: 59.6 MB (6.22%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,45 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        visit = set()
+        
+        ROW, COL = len(board), len(board[0])
+        
+        dx = [-1,1,0,0]
+        dy = [0,0,1,-1]
+                    
+                
+        def dfs(r,c, capture=False):
+            if r<0 or c<0 or r>=ROW or c>=COL or (r,c) in visit or board[r][c] == "X":
+                return
+            visit.add((r,c))
+            if capture:
+                board[r][c] = "X"
+            dfs(r-1,c, capture)
+            dfs(r+1,c, capture)
+            dfs(r,c-1, capture)
+            dfs(r,c+1, capture)
+        
+        # visit un captured cells
+        for r in range(ROW):
+            dfs(r, 0)
+            dfs(r, COL-1)
+        for c in range(COL):
+            dfs(0, c)
+            dfs(ROW-1, c)
+        
+        # print(visit)
+            
+        # visit captured cells
+        for r in range(1, ROW):
+            for c in range(1, COL):
+                if (r,c) not in visit:
+                    dfs(r,c, True)
+        
+                    
+        
+        return board
+    
+        
