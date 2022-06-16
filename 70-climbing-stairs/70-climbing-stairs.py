2aefc0154c81477c4f0bfc42f26e5d884f91035b
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-16 09:15:53 +0600

    Time: 48 ms (35.12%), Space: 13.8 MB (95.95%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,10 +4,11 @@ class Solution:
         def countWays(n):
             if n == 0: return 1
             if n < 0: return 0
-            if n-1 not in dp:
-                dp[n-1] = countWays(n-1)
             if n-2 not in dp:
                 dp[n-2] = countWays(n-2)
+            if n-1 not in dp:
+                dp[n-1] = countWays(n-1)
+            
             return dp[n-1] + dp[n-2]
         return countWays(n)
         
