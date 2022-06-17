c698bbd2a8b7873fbf7507b1754820257804abb9
Mehedi Hasan <mehedi1055@gmail.com>
2022-06-17 19:16:59 +0600

    Time: 35 ms (84.03%), Space: 13.9 MB (18.90%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,12 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob = {}
+        dp = {}
         n = len(nums)
-        rob[n], rob[n+1] = 0, 0
-        
-        def getAmount(i):
-            if i not in rob:
-                rob[i] = max(getAmount(i+1), nums[i]+getAmount(i+2))
-            return rob[i]
-        
-        return getAmount(0)
+        dp[n] = 0 
+        dp[n+1] = 0
+        def find(i):
+            if i in dp:
+                return dp[i]
+            first = nums[i] + find(i+2)
+            sec = find(i+1)
+            dp[i] = max(first, sec)
+            return dp[i]
+        return find(0)
